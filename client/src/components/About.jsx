928bb5f8ddaba057ede28243aefe1d85313bed30
Aastha yadav <aasthayadav815@gmail.com>
2025-07-27 15:52:20 +0530

    Update About.jsx

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -2,7 +2,7 @@ import { motion } from 'framer-motion';
 
 function About() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-900 via-blue-200 to-blue-500">
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-[#141E30] to-[#243B55-] text-black">
       <motion.div
         initial={{opacity: 0, scale: 0.9}}
         animate={{opacity:1, scale: 1}}
