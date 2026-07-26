03078efd9050107b067ec7f1ea9d1f3d4a9b0403
Prajwal Tiwari <tiwariprajwal705@gmail.com>
2026-07-27 00:07:18 +0530

    bug fixed

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -9,7 +9,8 @@ export const requireAuth = (req, res, next)=>{
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
+        req.user = decoded;
+        next();
     } catch(err){
         return res.status(401).json({success: false, message: "Invalid or expired token"});
     }
